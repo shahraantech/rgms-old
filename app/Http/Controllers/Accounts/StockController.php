b81899e82d900f1dd5c500f91c573f7723993dae
salman Raza <Salman.r@theateam.pk>
2022-10-10 13:35:26 +0500

    abc

--- app/Http/Controllers/Accounts/StockController.php
+++ app/Http/Controllers/Accounts/StockController.php
@@ -12,12 +12,7 @@ class StockController extends Controller
     public function index(Request $request)
     {
 
-//         $data['stock'] = Purchase::join('products','products.id','purchases.item_id')
-////            ->where('products.cat_id',1)
-//             ->orderBy('purchases.id','DESC')
-//            ->get();
          $data['stock'] =Purchase::with('products')->get();
-
         return view('accounts.stock.index')->with(compact('data'));
     }
 
